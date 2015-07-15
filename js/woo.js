fe3f3fd2c8664f0817598686a2d1cdd4d1bf3c46
Hanks <zhangyuhan@nashangban.com>
2015-07-15 19:11:53 +0800

    init page

---
+++ js/woo.js
@@ -0,0 +1,6 @@
+var header = document.querySelector('.header');  
+var origOffsetY = header.offsetTop;  
+function onScroll(e) {  
+    window.scrollY >= origOffsetY ? header.classList.add('sticky') : header.classList.remove('sticky'); 
+}
+document.addEventListener('scroll', onScroll);  
